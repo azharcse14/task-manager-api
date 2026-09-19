455386aa252ddf68217181176d13edbbadd5ed77
Azharul Islam <mdazharcse14@gmail.com>
2026-09-19 13:49:05 +0600

    Add HTTP server with health endpoint

--- main.go
+++ main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /health", healthHandler)
+
+	fmt.Println("Listening on :8080")
+	http.ListenAndServe(":8080", mux)
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Server is Alive")
+}
